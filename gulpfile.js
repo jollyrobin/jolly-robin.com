00dd20d7d01835cb79d2f2f5a77545631b6b6a90
Peter Duncan <peter@jolly-robin.com>
2016-11-18 09:32:03 +0000

    Updated build to move the files to a dist folder and added a gh-pages deploy task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,30 @@
 var gulp = require('gulp');
+var ghPages = require('gulp-gh-pages');
+var browserSync = require('browser-sync').create();
 
 gulp.task('default', function(){
 	console.log('Hello')
-})
+});
+
+gulp.task('build', function(){
+	gulp.src('app/*.html')
+		.pipe(gulp.dest('dist'));
+});
+
+// Static server
+gulp.task('serve', ['build'], function() {
+	browserSync.init({
+		server: {
+			baseDir: "./dist"
+		}
+	});
+
+	gulp.watch("app/**/*", ['build']);
+
+	gulp.watch("dist/**/*").on('change', browserSync.reload);
+});
+
+gulp.task('deploy', function() {
+  return gulp.src('./dist/**/*')
+    .pipe(ghPages());
+});
